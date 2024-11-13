b0055221721e6878c3b54e850a43a6f62dbdbc97
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-11-13 18:24:04 +0530

    Create CategoryRouter.js

---
+++ backend/routes/api/v2/CategoryRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const categoryController = require('../../../controllers/v2/CategoryController');
+
+router.get('/', categoryController.getAllCategories);
+
+module.exports = Router;
